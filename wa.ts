392a41ec3271320d7ea831b765f5ec60b0ca38ec
John Berthels <john.berthels@gmail.com>
2024-07-16 08:00:37 +0100

    factor in the filtering too

--- wa.ts
+++ wa.ts
@@ -3,36 +3,44 @@ import { pipe } from "fp-ts/function";
 
 type Ctor<T> = new (...args: any[]) => T;
 
-const isElt = <T extends HTMLElement>(
-    filterType: Ctor<T>
-): ((elt: HTMLElement) => elt is T) => {
-    return (elt): elt is T => elt instanceof filterType;
-};
+const isElt =
+    <T extends HTMLElement>(
+        filterType: Ctor<T>
+    ): ((elt: HTMLElement) => elt is T) =>
+    (elt): elt is T =>
+        elt instanceof filterType;
 
 const isButton = isElt(HTMLButtonElement);
 const isAudio = isElt(HTMLAudioElement);
 
-const getElement = (id: string): E.Either<string, HTMLElement> =>
-    pipe(
-        document.getElementById(id),
-        E.fromNullable(`No element with id ${id}`)
-    );
+const getElement =
+    <T extends HTMLElement>(
+        filter: (e: HTMLElement) => e is T,
+        wrongTypeErr: string
+    ): ((id: string) => E.Either<string, T>) =>
+    (id) =>
+        pipe(
+            document.getElementById(id),
+            E.fromNullable(`No element with id ${id}`),
+            E.filterOrElse(filter, () => wrongTypeErr)
+        );
 
 const playClicked = () => console.log("play clicked");
 
 const initialise = (): E.Either<string, any> => {
     const pb = pipe(
         "play-button",
-        getElement,
-        E.filterOrElse(isButton, () => "Not a button"),
+        getElement(isButton, "Not a button"),
+        //        E.filterOrElse(isButton, () => "Not a button"),
         E.map((b) => (b.onclick = playClicked))
     );
 
     const ae = pipe(
         "the-audio",
-        getElement,
-        E.filterOrElse(isAudio, () => "Not audio element")
+        getElement(isAudio, "Not audio element")
+        //        E.filterOrElse(isAudio, () => "Not audio element")
     );
+    console.log(`${pb} ${ae}`);
 
     return E.right("Got button and audio");
     //    const audio = pipe("the-audio", getElement, E.map(isAudio));
